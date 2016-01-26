b3168b893688042e96d0f1ffdd56bc2b09049ddf
Calum-Campbell <campbell.cj@hotmail.co.uk>
2016-01-26 12:50:14 +0000

    added text box to carers name, and click off keypoarb on background click

--- Dementia Onboarding/Dementia Onboarding/ViewController.swift
+++ Dementia Onboarding/Dementia Onboarding/ViewController.swift
@@ -2,6 +2,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
+    @IBOutlet weak var carersName: UITextField!
+    
+    //built in method
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
