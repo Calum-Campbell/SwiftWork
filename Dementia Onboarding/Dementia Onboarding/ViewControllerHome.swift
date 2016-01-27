5005a32592a6803f7e3d5579264c72c97bf9da82
Calum-Campbell <campbell.cj@hotmail.co.uk>
2016-01-27 14:09:58 +0000

    attempting some UISwipeRecongnition Changes, commeted out currently

---
+++ Dementia Onboarding/Dementia Onboarding/ViewControllerHome.swift
@@ -0,0 +1,39 @@
+
+
+import UIKit
+
+class ViewControllerHome: UIViewController {
+
+   
+ /*   @IBOutlet var swipe: UISwipeGestureRecognizer!
+    
+    @IBOutlet weak var imageView: UIImageView!
+ 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes"))
+        
+        
+        rightSwipe.direction = .Right
+        
+        view.addGestureRecognizer(rightSwipe)
+        
+        
+    }
+    
+    
+    func handleSwipes(sender: UISwipeGestureRecognizer){
+        if (sender.direction == .Right) {
+            imageView.image = UIImage(named: "slider2.png")
+        }
+    }
+*/
+
+    
+
+
+}
